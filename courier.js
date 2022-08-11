d2f4b6b2256708aa61791e916d5666dc7e233a08
Naman Kaushik <kaushiknaman.dk@gmail.com>
2022-08-11 13:25:02 +0530

    email template modified

--- courier.js
+++ courier.js
@@ -1,19 +1,19 @@
 import { CourierClient } from "@trycourier/courier";
+import * as dotenv from 'dotenv';
+dotenv.config();
+const courier = CourierClient({ authorizationToken: process.env.COURIERJS });
 
-const courier = CourierClient({ authorizationToken: "pk_test_G21K56A1ZVMG8FGFCM2BBSTNYY8C" }); // get from the Courier UI
-
-// Example: send a basic message to an email recipient
 const { requestId } = await courier.send({
   message: {
     to: {
       data: {
-        name: "Lol",
+        name: "Naman",
       },
       email: "gameboi1608@gmail.com",
     },
     content: {
-      title: "Back to the Future",
-      body: "Oh my {{name}}, we need 1.21 Gigawatts!",
+      title: "Welcome to RevereLabs!",
+      body: "Congratulations {{name}}!,\n Your account has been created with Revere Labs!",
     },
     routing: {
       method: "single",
